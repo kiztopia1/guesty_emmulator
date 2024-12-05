78787026b7647ce69243f3f17502aa17ecd05856
kirubel demelash <62418642+kiztopia1@users.noreply.github.com>
2024-12-05 10:19:39 +0300

    pocketbase added

--- api/index.js
+++ api/index.js
@@ -242,6 +242,6 @@ app.get("/api/conversations", async (req, res) => {
   }
 });
 
-app.listen(3000, () => console.log("Server ready on port 3001."));
+app.listen(3000, () => console.log("Server ready on port 3000."));
 
 module.exports = app;
